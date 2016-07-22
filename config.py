0ca0e07816a3cbf80e84760cadb389353d933f04
overborn <forkirill@i.ua>
2016-07-22 03:04:25 +0300

    fixes for heroku

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 CSRF_ENABLED = True
 SECRET_KEY = '4p((iuz0xcp8_1q3e*qfwn3*=60_83(etr_7+2*pvx)i(xc&34'
