03bfa15107c99d27ff0c821e61b7e2bd83b757c3
overborn <forkirill@i.ua>
2016-08-01 21:53:40 +0300

    logging for heroku added

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-import logging
+import logging, os
 from logging.handlers import RotatingFileHandler
 
 from flask import Flask
@@ -9,10 +9,21 @@ import wtforms_json
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
 wtforms_json.init()
 
-handler = RotatingFileHandler('foo.log', maxBytes=10000, backupCount=1)
+# Logging settings
+if os.environ.get('HEROKU') is None:
+    handler = RotatingFileHandler(
+        'testshop.log', maxBytes=1 * 1024 * 1024, backupCount=1
+    )
+    handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+else:
+    handler = logging.StreamHandler()
 handler.setLevel(logging.INFO)
 app.logger.addHandler(handler)
+app.logger.setLevel(logging.INFO)
+app.logger.info('testshop startup')
 
 from app import views, models
